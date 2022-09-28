7db4c25fbef9f1c2d8f8e3fe4a7cf0a62b2525c3
Cesar L (Kubano) Diaz <cesard@pbwweb.com>
2022-09-28 17:13:29 -0400

    initial project setup

---
+++ src/pha-platform/src/App.js
@@ -0,0 +1,33 @@
+import logo from './pbw-logo-new.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+      <div className="col">
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="img-fluid" alt="logo" />
+        <h1 className="text-center">
+            PHA Platform
+         </h1>
+         <h3>
+          We are Coming Soon...
+         </h3>
+         <small className="text-muted fs-5">
+          Email us at: <br />
+          <strong>
+          support@pbwweb.com
+          </strong>
+         </small>
+      </header>
+     
+    </div>
+      </div>
+    
+    </div></div>
+  );
+}
+
+export default App;
